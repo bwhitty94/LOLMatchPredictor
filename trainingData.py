cfb1c7442441b97468831708ce4c49e117d313b7
Juan <jdcerda@calpoly.edu>
2017-11-09 16:10:59 -0800

    Something else

--- trainingData.py
+++ trainingData.py
@@ -1,67 +1,62 @@
 import json
+import requests
 from pprint import pprint
 import matchCollection
 import cassiopeia as cass
 import leagues
 
+def requestSummonerData(region, summonerName, APIKey):
+    URL = "https://" + region + ".api.riotgames.com/lol/summoner/v3/summoners/by-name/" + summonerName + "?api_key=" + APIKey
+    response = requests.get(URL)
+    return response.json()
+
+def requestRankedData(region, ID, APIKey):
+    URL = "https://" + region + ".api.riotgames.com/lol/league/v3/leagues/by-summoner/" + ID + "?api_key=" + APIKey
+    response = requests.get(URL)
+    return response.json()
 
 with open('Data\matches1.json') as data_file:
     data = json.load(data_file)
     duration = data["matches"][0]["gameDuration"]
+    matches = data["matches"]
     Id = data["matches"][0]
+    region = "na1"
+    APIKey = "RGAPI-4e8e60d9-da17-425a-bdd5-15ac360a2a13"
 
     ## Ranks
     ## [Bronze,Silver,Gold,Platinum,Diamond,Master,Challenger] = [1,2,4,8,16,32,64]
     ## The first three piece of information will go into match collection to look at the history of that person
-    participantIdentities = data["matches"][0]["participantIdentities"]
-    participant = data["matches"][0]["participants"]
+    participantIdentities = matches[0]["participantIdentities"]
+    participant = matches[0]["participants"]
     summonerName = []
     summonerId = []
     accountId = []
     championId = []
-    summoner_list1 = [[]]
-
 
     for i in range(0,10):
         summonerName.append(participantIdentities[i]["player"]["summonerName"]) ## gets summoner name from a match
         summonerId.append(participantIdentities[i]["player"]["summonerId"]) ##gets summonerID allows us to then fetch there past information
         accountId.append(participantIdentities[i]["player"]["accountId"]) ## gets account ID from a match
-        #summonerRank = data["matches"][0]["participants"][0]["highestAchievedSeasonTier"] # gets the Summoner Rank
         championId.append(participant[i]["championId"])  # gets champion ID number
-    print(summonerName)
+        #summonerRank = data["matches"][0]["participants"][0]["highestAchievedSeasonTier"] # gets the Summoner Rank
 
+    summoner_list1 = [summonerName, summonerId, accountId, championId]
+    print(summoner_list1)
+
+    for i in  range(0, 10):
+        summonerData = requestSummonerData(region, summonerName[i], APIKey)
+        rankedData = requestRankedData(region, str(summonerId[i]), APIKey)
+        wins = rankedData[0]['entries'][1]['wins']
+        losses = rankedData[0]['entries'][1]['losses']
+        win_ratio = wins / (wins + losses)
+        print("wins:" + '{:4}'.format(str(wins)) + " losses:" + '{:4}'.format(str(losses))+ " win ratio:" + str(win_ratio))
+
+    ##check who won
+    # blue_team = data["matches"][0]["teams"][0]["win"]
     #
-    #summoner_list1[0].append(summonerName[0])
-    # summoner_list1.append(summonerId[0])
-    # summoner_list1.append(accountId[0])
-    # summoner_list1.append(championId[0])
+    # if blue_team == 'Win':
+    #     blue_team = 1
+    # else :
+    #     blue_team = 0
 
-    for j in range(0,0):
-        for i in range(0,10):
-             summoner_list1[j][i] = summonerName[i]
-    #         summoner_list1[j].append(summonerId[0])
-    #         summoner_list1[j].append(accountId[0])
-    #         summoner_list1[j].append(championId[0])
-    print(summoner_list1[0])
-
-    #name = cass.get_summoner(name= summonerName[3])
-
-
-
-
-    leagues.print_leagues(summonerName[0],"NA")
-    leagues.print_leagues(summonerName[1], "NA")
-    leagues.print_leagues(summonerName[2], "NA")
-   # leagues.print_leagues(summonerName[3], "na1") ## I believe he does not play in NA throwing an error...
-    leagues.print_leagues(summonerName[1], "NA")
-    leagues.print_leagues(summonerName[1], "NA")
-
-    # matchCollection.print_newest_match(summonerName, accountId, summonerId, "NA")
-    blue_team = data["matches"][0]["teams"][0]["win"]
-
-    if blue_team == 'Win':
-        blue_team = 1
-    else :
-        blue_team = 0
-
-pprint(summoner_list1)
+    # pprint(summoner_list1)
