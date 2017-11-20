4e6986903abc3a9da0b6a685927d3cef8cad3502
Kevin Gasik <kevin@theinternx.com>
2017-11-20 15:00:27 -0500

    Uploaded a program to use the front end information API CALL to put things in the correct format for a current match. Need to get a Run Keras function in this file.

--- connectBens.py
+++ connectBens.py
@@ -1,10 +1,5 @@
-import json
 import requests
 import time
-from pprint import pprint
-import matchCollection
-import cassiopeia as cass
-import leagues
 
 def requestSummonerData(region, summonerName, APIKey):
     URL = "https://" + region + ".api.riotgames.com/lol/summoner/v3/summoners/by-name/" + summonerName + "?api_key=" + APIKey
@@ -16,40 +11,30 @@ def requestRankedData(region, ID, APIKey):
     response = requests.get(URL)
     return response.json()
 
-def connectStuff(name: str , region: str):
-    #region = "na1"
-    APIKey = "RGAPI-02519475-aee0-4911-be95-d69941921f3a"
-    newfile = open("newfile.txt", "w+")
+def connectStuff(team):
+    summonerId = []
+    #this is the format of whats coming in "team"
+    #champions.append({"summoner": participant.summoner.name, "id": champ.id, "name": champ.name, "imageUrl": image})
+    for e in range(0,10):
+        summonerId.append(team[e]["summonerId"])
+    region = "na1"
+    APIKey = "RGAPI-7d5fb1a7-399c-4289-9801-54d1dde10d3e"
+    currentmatchfile = open("currentmatchfile.txt", "w+")
     number_matches = 0
-    summoner = requestSummonerData(region, str(name), APIKey)
-    print(summoner)
+
 
     for j in range(0,1):
 
-        summonerName = []
-        summonerId = []
-        accountId = []
-        championId = []
         tmpString = ""
         isError = False
 
-        #for i in range(0,10):
-           #summonerName.append(summ) ## gets summoner name from a match
-           #summonerId.append(participantIdentities[i]["player"]["summonerId"]) ##gets summonerID allows us to then fetch there past information
-           #accountId.append(participantIdentities[i]["player"]["accountId"]) ## gets account ID from a match
-           #championId.append(participant[i]["championId"])  # gets champion ID number
-           #summonerRank = data["matches"][0]["participants"][0]["highestAchievedSeasonTier"] # gets the Summoner Rank
-
-        summonerId = summoner["id"]
-        #print(summoner_list1)
-
-        for i in  range(0, 1):
+        for i in  range(0, 10):
             rankedData = requestRankedData(region, str(summonerId[i]), APIKey)
             k = 0
             isId = False
             while (isId == False  ):
                 try:
-                    if(int(rankedData[0]['entries'][k]['playerOrTeamId']) == summonerId[i]):
+                    if(int(rankedData[0]['entries'][k]['playerOrTeamId']) == int(summonerId[i])):
                         isId = True
                 except (IndexError):
                     k += 1
@@ -63,40 +48,52 @@ def connectStuff(name: str , region: str):
                    isId = True
             k -= 1
             try:
-             #wins = rankedData[0]['entries'][k]['wins']
-             #losses = rankedData[0]['entries'][k]['losses']
-             tier = rankedData[0]['tier']
-             rank = rankedData[0]['entries'][k]['rank']
+             wins = rankedData[0]['entries'][k]['wins']
+             losses = rankedData[0]['entries'][k]['losses']
+             #tier = rankedData[0]['tier']
+             #rank = rankedData[0]['entries'][k]['rank']
             except (IndexError):
                 tmpString = ""
                 print('found an IndexError')
                 isError = True;
                 continue
-            #win_ratio = wins / (wins + losses)
-            #print("wins:" + '{:4}'.format(str(wins)) + " losses:" + '{:4}'.format(str(losses))+ " win ratio:" + str(win_ratio))
-
-            print("Tier:" + '{:5}'.format(str(tier)) + " Rank:" + '{:5}'.format(str(rank)))
-            tmpString += ('{0:.6}'.format(str(tier)) + ",")
+            win_ratio = wins / (wins + losses)
+            print("wins:" + '{:4}'.format(str(wins)) + " losses:" + '{:4}'.format(str(losses))+ " win ratio:" + str(win_ratio))
+            #print("Tier:" + '{:5}'.format(str(tier)) + " Rank:" + '{:5}'.format(str(rank)))
+            tmpString += ('{0:.6}'.format(str(win_ratio)) + ",")
 
         if (isError):
             continue
-        newfile.write(tmpString)
+        currentmatchfile.write(tmpString)
         print(tmpString)
 
-
-        blue_team = data["matches"][j]["teams"][0]["win"]
-        result = 1 if blue_team == 'Win' else 0
-        newfile.write(str(result) + "\n")
-
-
-        print(j)
         number_matches += 1
-        print(number_matches)
+        print("We went through the first five players Ben: Successful Exit")
         time.sleep(2)
+
+
+ # keep this in incase you want to change this function
         if( number_matches >= 8 ):
             number_matches = 0
             time.sleep(130)
 
-
 if __name__ == "__main__":
-    connectStuff("Kalturi", "na1")
+    #[['TastyKakeKing2', 'xouchiesssx', 'hautala147', 'xPÃ\xadnny', 'McLovin1500', 'CypherNek', 'Ã?eath MÃ£rk',
+    #  'Monkeyfightr', 'JasonXD', 'xoMajesty'],
+    # [57005769, 19592759, 23035037, 38804535, 34202044, 36041723, 87822154, 30942910, 22668710, 32333042],
+    # [218739531, 32286644, 36991143, 201694529, 48591434, 50650413, 239003617, 43053794, 36542700, 47191301],
+    # [240, 29, 63, 64, 1, 24, 238, 432, 51, 17]]
+    # champions.append({"summoner": participant.summoner.name, "id": champ.id, "name": champ.name, "imageUrl": image})
+    myTeamTest = []
+    myTeamTest.append({"summoner": "TastyKakeKing2", "id": 240, "name": "NoClue1","imageUrl": "image1", "summonerId": "57005769" })
+    myTeamTest.append({"summoner": "xouchiesssx", "id": 29, "name": "NoClue2","imageUrl": "image2", "summonerId": "19592759"})
+    myTeamTest.append({"summoner": "hautala147", "id": 63, "name": "NoClue3", "imageUrl": "image34", "summonerId": "23035037"})
+    myTeamTest.append({"summoner": "McLovin1500", "id": 1, "name": "NoClue4", "imageUrl": "image4", "summonerId": "34202044"})
+    myTeamTest.append({"summoner": "CypherNek", "id": 24, "name": "NoClue5", "imageUrl": "image5", "summonerId": "36041723"})
+    myTeamTest.append({"summoner": "TastyKakeKing2", "id": 240, "name": "NoClue1", "imageUrl": "image1", "summonerId": "57005769"})
+    myTeamTest.append({"summoner": "xouchiesssx", "id": 29, "name": "NoClue2", "imageUrl": "image2", "summonerId": "19592759"})
+    myTeamTest.append({"summoner": "hautala147", "id": 63, "name": "NoClue3", "imageUrl": "image34", "summonerId": "23035037"})
+    myTeamTest.append({"summoner": "McLovin1500", "id": 1, "name": "NoClue4", "imageUrl": "image4", "summonerId": "34202044"})
+    myTeamTest.append({"summoner": "CypherNek", "id": 24, "name": "NoClue5", "imageUrl": "image5", "summonerId": "36041723"})
+    print(myTeamTest)
+    connectStuff(myTeamTest)
