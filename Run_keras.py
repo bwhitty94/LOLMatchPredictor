2c9ada338386ba824586d16f8d8aade75ce769aa
Juan <jdcerda@calpoly.edu>
2017-11-21 16:41:05 -0800

    added keras file

--- Run_keras.py
+++ Run_keras.py
@@ -5,14 +5,13 @@ import numpy
 def runKeras():
     numpy.random.seed(7)
     # load pima indians dataset
-    dataset = numpy.loadtxt("newfile.txt", delimiter=",")
-    datatest = numpy.loadtxt("currentmatchfile.txt", delimiter=",")
+    dataSet = numpy.loadtxt("testfile.txt", delimiter=",")
+    dataTest = numpy.loadtxt("currentmatchfile.txt", delimiter=",")
     # split into input (X) and output (Y) variables
-    X_training = dataset[0:100,0:10]
-    Y_training = dataset[0:100,10]
-    X_testing= datatest[0:,0:9]
+    X_training = dataSet[0:100,0:10]
+    Y_training = dataSet[0:100,10]
+    X_testing = dataTest[0:10]
     #Y_testing = datatest[0:,10]
-
 
     # create model
     model = Sequential()
@@ -25,9 +24,10 @@ def runKeras():
     model.fit(X_training, Y_training, epochs=10, batch_size=5)
     # evaluate the model
     #scores = model.evaluate(X_testing, Y_testing)
-    predictions = model.predict(X_testing)
+    # predictions = model.predict(numpy.array([[0.9300,0.9581,0.8649,0.7080,0.6215,0.1300,0.0581,0.0649,0.0080,0.0215]]))
+    predictions = model.predict(numpy.array(X_testing, ndmin = 2))
+    print (predictions)
     #print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
-    return print("\n%s: %.2f%%" % (model.metrics_names[1], predictions[1]*100))
 
 if __name__ == "__main__":
     runKeras()
