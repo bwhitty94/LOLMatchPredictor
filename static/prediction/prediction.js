df91a3833a1fc358017975ca4cee526df28b606f
Ben Whittle <b.whitty94@gmail.com>
2017-11-26 17:50:31 -0800

    Added localStorage to persist prediction through refresh

--- static/prediction/prediction.js
+++ static/prediction/prediction.js
@@ -2,9 +2,17 @@
 var app = angular.module('LoLMP');
 
 app.controller('prediction', function($scope, $location, prediction, $document) {
-    $scope.summoner = prediction.summoner;
-    $scope.blueTeam = prediction.blueTeam;
-    $scope.redTeam = prediction.redTeam;
+    $scope.summoner = prediction.summoner || JSON.parse(localStorage.getItem("summoner"));
+    $scope.blueTeam = prediction.blueTeam || JSON.parse(localStorage.getItem("blueTeam"));
+    $scope.redTeam = prediction.redTeam || JSON.parse(localStorage.getItem("redTeam"));
+
+    localStorage.setItem("summoner", JSON.stringify($scope.summoner));
+    localStorage.setItem("blueTeam", JSON.stringify($scope.blueTeam));
+    localStorage.setItem("redTeam", JSON.stringify($scope.redTeam));
+
+    if (!$scope.summoner) {
+        $location.path('/');
+    }
 
     $scope.$on('$viewContentLoaded', function(event) {
         $('.prediction').attr("style", "display: inline");
